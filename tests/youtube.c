f207b1e76e5e01c4d6d875f1b8b5bed9bc979660
legatvs <legatvs@gmail.com>
2009-07-04 12:46:11 +0300

    Clean up: tests/youtube.c

--- tests/youtube.c
+++ tests/youtube.c
@@ -3,8 +3,6 @@
 
 #include "test.h"
 
-#define TEST_URL "http://www.youtube.com/watch?v=d0j_ONmVcXA"
-
 static const char *
 formats[] = {
     "flv",
@@ -15,34 +13,31 @@ formats[] = {
     NULL
 };
 
+static const char *
+urls[] = {
+    "http://www.youtube.com/watch?v=d0j_ONmVcXA",
+    "http://www.youtube.com/v/d0j_ONmVcXA",
+    "http://www.youtube-nocookie.com/v/3PuHGKnboNY",
+    "http://www.last.fm/music/Rammstein/+videos/+1-3jwXQFFLSHo",
+};
+
 int
 main (int argc, char *argv[]) {
-    register int i,rc=0;
+    register int i, rc;
     char *cmd;
 
-    for (i=0; formats[i]; ++i) {
+    for (i=0,rc=0,cmd=0; formats[i] && !rc; ++i) {
         asprintf(&cmd, "../src/cclive -nf %s", formats[i]);
-        rc = runtest_host(cmd, TEST_URL);
+        rc = runtest_host(cmd, urls[0]);
         free(cmd);
-        if (rc > 0)
-            break;
     }
 
-    if (rc == 0) {
-        rc = runtest_host("../src/cclive -n",
-            "http://www.youtube.com/v/d0j_ONmVcXA");
-    }
-
-    if (rc == 0) {
-        rc = runtest_host("../src/cclive -n",
-            "http://www.youtube-nocookie.com/v/3PuHGKnboNY");
-    }
-
-    if (rc == 0) {
-        rc = runtest_host("../src/cclive -n",
-            "http://www.last.fm/music/Rammstein/+videos/+1-3jwXQFFLSHo");
+    for (i=0,cmd=0; i<urls[i] && !rc; ++i) {
+        asprintf(&cmd, "../src/cclive -n %s", urls[i]);
+        rc = runtest_host(cmd, urls[i]);
+        free(cmd);
     }
 
     return(rc);
-
 }
+
