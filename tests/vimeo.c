57515dc95ef80e37dc82e8c0049cbbc19fceba3e
legatvs <legatvs@gmail.com>
2009-07-04 12:53:02 +0300

    Clean up: tests/vimeo.c

--- tests/vimeo.c
+++ tests/vimeo.c
@@ -11,14 +11,14 @@ static const char *formats[] = {
 
 int
 main (int argc, char *argv[]) {
-    register int i,rc=0;
+    register int i,rc;
     char *cmd;
-    for (i=0; formats[i]; ++i) {
+
+    for (i=0,rc=0,cmd=0; formats[i] && !rc; ++i) {
         asprintf(&cmd, "../src/cclive -nf %s", formats[i]);
         rc = runtest_host(cmd, "http://vimeo.com/1485507");
         free(cmd);
-        if (rc != 0)
-            break;
     }
+
     return (rc);
 }
