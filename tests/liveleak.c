241e98c7ed6f00e8c5dbfb79fb8889d3d59bf215
legatvs <legatvs@gmail.com>
2009-07-04 12:51:57 +0300

    Clean up: tests/liveleak.c

--- tests/liveleak.c
+++ tests/liveleak.c
@@ -2,7 +2,8 @@
 
 #include "test.h"
 
-static const char *urls[] = {
+static const char
+*urls[] = {
     "http://www.liveleak.com/view?i=704_1228511265",
     "http://www.liveleak.com/e/6ff_1228698283",
     NULL
@@ -10,11 +11,10 @@ static const char *urls[] = {
 
 int
 main (int argc, char *argv[]) {
-    register int i,rc=0;
-    for (i=0; urls[i]; ++i) {
+    register int i,rc;
+
+    for (i=0,rc=0; urls[i] && !rc; ++i)
         rc = runtest_host("../src/cclive -n", urls[i]);
-        if (rc != 0)
-            break;
-    }
+
     return(rc);
 }
