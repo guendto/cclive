e0967b4b5c214d8a07e26c76f8bef610e25ef07b
legatvs <legatvs@gmail.com>
2009-07-04 12:52:24 +0300

    Clean up: tests/sevenload.c

--- tests/sevenload.c
+++ tests/sevenload.c
@@ -12,12 +12,11 @@ NULL
 
 int
 main (int argc, char *argv[]) {
-    register int i,rc=0;
-    for (i=0; urls[i]; ++i) {
+    register int i,rc;
+
+    for (i=0,rc=0; urls[i] && !rc; ++i)
         rc = runtest_host("../src/cclive -n", urls[i]);
-        if (rc != 0)
-            break;
-    }
+
     return(rc);
 }
 
