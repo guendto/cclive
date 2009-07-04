903a390a9774894e284150e8be158dc22c68d0a1
legatvs <legatvs@gmail.com>
2009-07-04 12:50:14 +0300

    Clean up: tests/evisor.c

--- tests/evisor.c
+++ tests/evisor.c
@@ -4,8 +4,11 @@
 
 int
 main (int argc, char *argv[]) {
-    int rc = runtest_host("../src/cclive -n",
+    int rc;
+    
+    rc = runtest_host("../src/cclive -n",
         "http://www.evisor.tv/tv/rennstrecken/"
         "1-runde-oschersleben-14082008--6985.htm");
+
     return (rc);
 }
