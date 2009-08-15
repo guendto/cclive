eb2123e2443c43fc651d7750064fe3f77358c699
legatvs <legatvs@gmail.com>
2009-08-15 23:48:13 +0300

    redtube: new test link.

--- tests/redtube.c
+++ tests/redtube.c
@@ -4,8 +4,8 @@
 
 int
 main (int argc, char *argv[]) {
-    int rc = runtest_host(NULL, "http://www.redtube.com/115732");
+    int rc = runtest_host(NULL, "http://www.redtube.com/3644");
     if (rc == 0)
-        rc = runtest_host(NULL, "http://www.redtube.com/embed/115732");
+        rc = runtest_host(NULL, "http://www.redtube.com/embed/3644");
     return (rc);
 }
