cb1b2c2f73a1ae1a40feb2eb417791b6e05abbdf
legatvs <legatvs.git@gmail.com>
2009-09-27 02:05:14 +0300

    Add: w32/ subdir.

---
+++ w32/make-w32.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+HOST=i486-mingw32 # FreeBSD: mingw32
+PREFIX="`pwd`/dist"
+
+CURL_CONFIG=\
+"/home/legatvs/src/non-installed/curl-7.19.6/tmp/dist/bin/curl-config"
+
+PCRE_CONFIG=\
+"/home/legatvs/src/non-installed/pcre-7.9/tmp/dist/bin/pcre-config"
+
+ICONV_PREFIX=\
+"/home/legatvs/src/non-installed/libiconv-1.13.1/tmp/dist"
+
+export CXXFLAGS="-Os -pipe -march=i686 -fno-strict-aliasing"
+
+export libcurl_CFLAGS="`$CURL_CONFIG --cflags`"
+export libcurl_LIBS="`$CURL_CONFIG --libs`"
+
+export libpcrecpp_CFLAGS="`$PCRE_CONFIG --cflags`"
+export libpcrecpp_LIBS="`$PCRE_CONFIG --libs` -lpcrecpp"
+
+../configure --prefix="$PREFIX" --host="$HOST" \
+     --with-libiconv-prefix="$ICONV_PREFIX" --without-man #\
+#    && make install-strip
+
+
