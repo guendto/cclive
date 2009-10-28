32b49f570b00c1036c8e71ea2ac67ae2bda486e0
Toni Gundogdu <legatvs.git@gmail.com>
2009-10-28 09:10:26 +0200

    make-w32.sh: bump pcre to 8.0

--- w32/make-w32.sh
+++ w32/make-w32.sh
@@ -9,7 +9,7 @@ CURL_CONFIG=\
 "/home/legatvs/src/non-installed/curl-7.19.6/release/dist/bin/curl-config"
 
 PCRE_CONFIG=\
-"/home/legatvs/src/non-installed/pcre-7.9/release/dist/bin/pcre-config"
+"/home/legatvs/src/non-installed/pcre-8.00/release/dist/bin/pcre-config"
 
 ICONV_PREFIX=\
 "/home/legatvs/src/non-installed/libiconv-1.13.1/release/dist"
