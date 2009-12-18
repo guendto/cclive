a3dbaf49cb90d74525ef9fd61e1dce858c19bef9
Toni Gundogdu <legatvs.git@gmail.com>
2009-12-18 12:47:44 +0200

    tests: support MULTI_TEST.

--- tests/multi.cpp
+++ tests/multi.cpp
@@ -1,4 +1,5 @@
 #include "test.h"
+#include <cstdlib>
 
 #define TEST_URL \
     "http://break.com/index/beach-tackle-whip-lash.html"
@@ -7,6 +8,10 @@
     "http://www.youtube.com/watch?v=DeWsZ2b_pK4"
 
 MAIN_BEGIN
+    if (!getenv("MULTI_TEST")) {
+        std::cout << "SKIP: No multi test requested" << std::endl;
+        return 0;
+    }
 #ifndef HOST_W32
     return runtest_multi(TEST_URL, TEST_URL2);
 #else
