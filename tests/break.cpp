31eb94ba5c00aadee5eda7ed01a5d8932f486267
Toni Gundogdu <legatvs.git@gmail.com>
2010-01-07 16:38:30 +0200

    remove host tests.

--- tests/break.cpp
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-
-MAIN_BEGIN
-    return
-        runtest_host("http://break.com/index/beach-tackle-whip-lash.html");
-MAIN_END
-
-
--- tests/evisor.cpp
+++
@@ -1,9 +0,0 @@
-#include "test.h"
-
-MAIN_BEGIN
-    return runtest_host(
-        "http://www.evisor.tv/tv/rennstrecken/"
-        "1-runde-oschersleben-14082008--6985.htm");
-MAIN_END
-
-
