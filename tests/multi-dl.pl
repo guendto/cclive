4e1b8c913683b8305a09a7eeb70e83137d1085a5
Toni Gundogdu <legatvs.git@gmail.com>
2010-01-28 12:29:03 +0200

    multi-dl.pl: add -f, -O.

--- tests/multi-dl.pl
+++ tests/multi-dl.pl
@@ -1,5 +1,9 @@
 exit ccliveTest::run(
     "http://www.youtube.com/watch?v=DeWsZ2b_pK4",
     "http://break.com/index/beach-tackle-whip-lash.html",
-    "--remote" # implies internet requirement
+    "--remote", # implies internet requirement
+    "-f",
+    "3gp",
+    "-O",
+    "/dev/null"
 );
