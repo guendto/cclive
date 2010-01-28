7bc767da22a266f4abd36dab4e72aa284bdcd1cc
Toni Gundogdu <legatvs.git@gmail.com>
2010-01-28 13:43:11 +0200

    add NO_MULTIDL.

--- tests/multi-dl.pl
+++ tests/multi-dl.pl
@@ -1,3 +1,7 @@
+if ($ENV{NO_MULTIDL}) {
+    print STDERR "SKIP: No multi download.\n";
+    exit 0;
+}
 exit ccliveTest::run(
     "http://www.youtube.com/watch?v=DeWsZ2b_pK4",
     "http://break.com/index/beach-tackle-whip-lash.html",
