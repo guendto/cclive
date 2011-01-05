c31424523ed97b948e879482a37e41c894fcb4c1
Toni Gundogdu <legatvs.git@gmail.com>
2011-01-05 22:15:59 +0200

    makedist.sh: Add a warning

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# This script will commit ChangeLog temporarily so that git-archive
+# picks the updated ChangeLog to the tarball.
+
 project=cclive
 branch=master
 after="Thu Aug 12 15:54:58 2010"
+
+echo "WARNING!
+
+Please note that this script will call git-reset (--hard) during
+this process so be sure to commit your changes before you run this
+script.
+
+Last chance to bail out (CTRL-C) before we call 'git reset --hard'."
+read -s -n1
 
 descr=`git describe $branch`
 basename="$project-$descr"
@@ -10,9 +22,7 @@ tarball="$basename.tar.gz"
 
 echo ":: Generate $tarball"
 
-# Note: we commit ChangeLog (temporarily) changes so that git-archive
-# picks the generated changes. Perhaps there's a more convinient way.
-
+# TODO: Elegant way.
 git log --stat --after="$after" >ChangeLog && \
 git commit -q ChangeLog -m "Generate $tarball" && \
 git archive $branch --prefix="$basename/" | gzip >$tarball && \
