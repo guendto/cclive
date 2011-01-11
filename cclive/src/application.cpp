952cd21b638ca31714564e6a8bb1ce24c6b1d5f9
Toni Gundogdu <legatvs.git@gmail.com>
2011-01-11 13:54:11 +0200

    Add email to copyright lines

--- cclive/src/application.cpp
+++ cclive/src/application.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2010,2011 Toni Gundogdu.
+* Copyright (C) 2010,2011  Toni Gundogdu <legatvs@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
