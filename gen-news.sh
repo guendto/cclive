9101e1a1e407d6b0d79b086a88f04aa2af2ac7d5
Toni Gundogdu <legatvs@gmail.com>
2012-03-03 11:20:09 +0200

    Add gen-news.sh (repo only)

---
+++ gen-news.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# gen-news.sh for cclive
+# Copyright (C) 2012  Toni Gundogdu
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+set -e
+IFS=$'
+'
+a=`grep < ~/.gitconfig name | cut -f2 -d= | sed 's/^\s*//'`
+v=`./gen-ver.sh`
+d=`date +%F`
+b="$v ($d) / $a
+
+"
+t=`git tag | tail -n1`
+for a in `git log --format='%an' "$t"..HEAD | sort | uniq`; do
+  c=`git log --format="  - %s [%h]" --author="$a" "$t"..HEAD | sort -h`
+  b="$b$a:
+$c
+
+"
+done
+n=`cat NEWS`
+F=NEWS
+echo "$b"  >"$F" ; echo "$n" >>"$F"
