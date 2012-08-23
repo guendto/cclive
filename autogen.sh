5e9b65cabf5b207f4f110c8524f8e4c2bdbe3b39
Toni Gundogdu <legatvs@gmail.com>
2012-08-23 19:34:12 +0300

    autogen.sh: Add copyright notice

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+#
 # autogen.sh for cclive.
+# Copyright (C) 2012  Toni Gundogdu <legatvs@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+set -e
 
 source=.gitignore
 cachedir=autom4te.cache
@@ -49,6 +66,4 @@ do
 done
 
 echo "Generate configuration files..."
-autoreconf -if \
-    && gen_manual \
-      && echo "You can now run 'configure'"
+autoreconf -if && gen_manual && echo "You can now run 'configure'"
