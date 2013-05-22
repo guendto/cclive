guendto/cclive

commit 4b11837864952c01e0e5db080684f0307d864b0f

Use quvi compatibility layer
Other notable changes:

* multi-segmented media is no longer supported
    (unused and deprecated, libquvi 0.9 will no longer support
     this, either)

* --filename-format: %h sequence has been removed
    (removed also in libquvi 0.9)

Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

src/cc/get.cpp
@@ -15,8 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <iostream>
+#include <ccinternal>
 
+#include <iostream>
 #include <boost/program_options/variables_map.hpp>
 
 #include <ccoptions>
@@ -34,61 +35,55 @@ void get(quvi::media& media, void *curl)
 {
   const po::variables_map map = cc::opts.map();
 
-  const bool no_download = opts.flags.no_download;
   const int max_retries  = map["max-retries"].as<int>();
   const int retry_wait   = map["retry-wait"].as<int>();
+
+  const bool no_download = opts.flags.no_download;
   const bool exec        = map.count("exec");
 
-  quvi::url url;
-  int n = 0;
+  int retry = 0;
 
-  while ((url = media.next_url()).ok())
+  while (retry <= max_retries)
     {
-      int retry = 0;
-      ++n;
-      while (retry <= max_retries)
+      cc::file file(media);
+
+      if (file.nothing_todo())
         {
-          cc::file file(media, url, n);
-
-          if (file.nothing_todo())
-            {
-              if (exec)
-                cc::exec(file);
-
+          if (exec)
+            cc::exec(file);
 #define E_NOTHING_TODO "media retrieved completely already"
-              throw std::runtime_error(E_NOTHING_TODO);
+          throw std::runtime_error(E_NOTHING_TODO);
 #undef E_NOTHING_TODO
-            }
-
-          // Download media.
-
-          if (retry > 0)
-            {
-              cc::log
-                  << "Retrying "
-                  << retry
-                  << " of "
-                  << max_retries
-                  << " ... "
-                  << std::flush;
-
-              cc::wait(retry_wait);
-            }
-
-          cc::log << file.to_s(url) << std::endl;
-          ++retry;
-
-          if (!no_download)
-            {
-              if (!file.write(url, curl));
-                continue; // Retry.
-
-              if (exec)
-                cc::exec(file);
-            }
-
-          break; // Stop retrying.
         }
+
+      // Download media.
+
+      if (retry > 0)
+        {
+          cc::log
+              << "Retrying "
+              << retry
+              << " of "
+              << max_retries
+              << " ... "
+              << std::flush;
+
+          cc::wait(retry_wait);
+        }
+
+      cc::log << file.to_s(media) << std::endl;
+      ++retry;
+
+      if (!no_download)
+        {
+          if (!file.write(media, curl));
+          continue; // Retry.
+
+          if (exec)
+            cc::exec(file);
+        }
+
+      break; // Stop retrying.
     }
 }
 

src/cc/application.cpp
@@ -30,9 +30,9 @@
 #define foreach BOOST_FOREACH
 #endif
 
+#include <ccquvi>
 #include <ccapplication>
 #include <ccoptions>
-#include <ccquvi>
 #include <ccutil>
 #include <cclog>
 #include <ccre>
@@ -141,27 +141,6 @@ static void print_quvi_error(const quvi::error& e)
   cc::log << "libquvi: error: " << e.what() << std::endl;
 }
 
-static void check_quvi_error(const quvi::error& e)
-{
-  const long resp_code = e.response_code();
-
-  if (resp_code >= 400 && resp_code <= 500)
-    throw e;
-
-  else
-    {
-      switch (e.quvi_code())
-        {
-        case QUVI_CALLBACK:
-          print_quvi_error(e);
-          break; // Retry.
-
-        default:
-          throw e;
-        }
-    }
-}
-
 static const char depr_msg[] =
   "WARNING '--format list' is deprecated and will be removed in the later\n"
   "WARNING versions. Use --query-formats instead.";
@@ -245,7 +224,7 @@ query_formats(const quvi::query& query,
       {
         print_checking(i,n);
 
-        const std::string formats = query.formats(url, opts);
+        const std::string formats = query.streams(url, opts);
 
         print_done();
 
@@ -318,7 +297,7 @@ static void set_format_string(const std::string& url, quvi::options& qopts,
       if (map.count("prefer-format"))
         parse_prefer_format(url, fmt, map);
     }
-  qopts.format(fmt);
+  qopts.stream = fmt;
 }
 
 extern char LICENSE[]; // cc/license.cpp
@@ -372,6 +351,7 @@ application::exit_status application::exec(int argc, char **argv)
   // --support
 
   quvi::query query; // Throws quvi::error caught in main.cpp
+  query.setup_curl();
 
   if (opts.flags.support)
     {
@@ -439,8 +419,8 @@ application::exit_status application::exec(int argc, char **argv)
   cc::curl_setup(_curl);
 
   quvi::options qopts;
-  qopts.statusfunc(status_callback);
-  qopts.resolve(opts.flags.no_resolve);
+  qopts.resolve = ! opts.flags.no_resolve;
+  qopts.statusfunc = status_callback;
 
   // Seed random generator.
 
@@ -518,7 +498,10 @@ application::exit_status application::exec(int argc, char **argv)
               }
             catch(const quvi::error& e)
               {
-                check_quvi_error(e);
+                if (e.cannot_retry())
+                  throw e;
+                else
+                  print_quvi_error(e);
               }
 
             cc::get(m, _curl);

src/cc/background.cpp
@@ -1,5 +1,5 @@
 /* cclive
- * Copyright (C) 2010-2011  Toni Gundogdu <legatvs@gmail.com>
+ * Copyright (C) 2010-2013  Toni Gundogdu <legatvs@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@
 
 #include <boost/iostreams/tee.hpp>
 
+#include <ccquvi>
 #include <ccutil>
 #include <ccapplication>
 #include <cclog>

src/cc/file.cpp
@@ -46,8 +46,8 @@
 #include <curl/curl.h>
 #include <pcrecpp.h>
 
-#include <ccoptions>
 #include <ccquvi>
+#include <ccoptions>
 #include <ccprogressbar>
 #include <ccre>
 #include <ccutil>
@@ -59,12 +59,12 @@ namespace cc
 
 namespace po = boost::program_options;
 
-file::file(const quvi::media& media, const quvi::url& url, const int n)
+file::file(const quvi::media& media)
   : _initial_length(0), _nothing_todo(false)
 {
   try
     {
-      _init(media, url, n);
+      _init(media);
     }
   catch (const cc::nothing_todo_error&)
     {
@@ -126,9 +126,12 @@ static int progress_cb(void *ptr, double, double now, double, double)
   return reinterpret_cast<progressbar*>(ptr)->update(now);
 }
 
-static void _set(CURL *c, std::ofstream *o, progressbar *pb,
-                 const double initial_length)
+static void _set(const quvi::media& m, CURL *c, std::ofstream *o,
+                 progressbar *pb, const double initial_length)
 {
+  curl_easy_setopt(c, CURLOPT_URL, m.stream_url().c_str());
+  curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, write_cb);
+
   curl_easy_setopt(c, CURLOPT_PROGRESSFUNCTION, progress_cb);
   curl_easy_setopt(c, CURLOPT_PROGRESSDATA, pb);
   curl_easy_setopt(c, CURLOPT_NOPROGRESS, 0L);
@@ -156,11 +159,8 @@ static void _restore(CURL *c)
                    static_cast<curl_off_t>(0L));
 }
 
-bool file::write(const quvi::url& u, CURL *curl) const
+bool file::write(const quvi::media& m, CURL *curl) const
 {
-  curl_easy_setopt(curl, CURLOPT_URL, u.media_url().c_str());
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
-
   std::ios_base::openmode mode = std::ofstream::binary;
 
   if (opts.flags.overwrite)
@@ -184,8 +184,8 @@ bool file::write(const quvi::url& u, CURL *curl) const
       throw std::runtime_error(s);
     }
 
-  progressbar pb(*this, u);
-  _set(curl, &out, &pb, _initial_length);
+  progressbar pb(*this, m);
+  _set(m, curl, &out, &pb, _initial_length);
 
 #ifdef WITH_SIGNAL
   recv_usr1 = 0;
@@ -262,12 +262,12 @@ static double to_mb(const double bytes)
   return bytes/(1024*1024);
 }
 
-std::string file::to_s(const quvi::url& url) const
+std::string file::to_s(const quvi::media& m) const
 {
-  const double length = to_mb(url.content_length());
+  const double length = to_mb(m.content_length());
 
   boost::format fmt = boost::format("%s  %.2fM  [%s]")
-                      % _name % length % url.content_type();
+                      % _name % length % m.content_type();
 
   return fmt.str();
 }
@@ -284,9 +284,7 @@ static fs::path output_dir(const po::variables_map& map)
 
 typedef std::vector<std::string> vst;
 
-void file::_init(const quvi::media& media,
-                 const quvi::url& url,
-                 const int n)
+void file::_init(const quvi::media& media)
 {
   _title = media.title();
 
@@ -308,7 +306,7 @@ void file::_init(const quvi::media& media,
       _path           = p.string();
       _initial_length = file::exists(_path);
 
-      if ( _initial_length >= url.content_length() && ! opts.flags.overwrite)
+      if ( _initial_length >= media.content_length() && ! opts.flags.overwrite)
         throw cc::nothing_todo_error();
     }
 
@@ -341,8 +339,8 @@ void file::_init(const quvi::media& media,
 
       pcrecpp::RE("%i").GlobalReplace(media.id(), &fname_format);
       pcrecpp::RE("%t").GlobalReplace(title, &fname_format);
-      pcrecpp::RE("%s").GlobalReplace(url.suffix(), &fname_format);
-      pcrecpp::RE("%h").GlobalReplace(media.host(), &fname_format);
+      pcrecpp::RE("%h").GlobalReplace("nohostseq", &fname_format);
+      pcrecpp::RE("%s").GlobalReplace(media.file_ext(), &fname_format);
 
       if (map.count("subst")) // Deprecated.
         {
@@ -364,10 +362,6 @@ void file::_init(const quvi::media& media,
       std::stringstream b;
 
       b << fname_format;
-
-      // A multi-segment media.
-
-      if (n > 1) b << "_" << n;
 
       // Output dir.
 
@@ -396,7 +390,7 @@ void file::_init(const quvi::media& media,
               if (_initial_length == 0)
                 break;
 
-              else if (_initial_length >= url.content_length())
+              else if (_initial_length >= media.content_length())
                 throw cc::nothing_todo_error();
 
               else

src/cc/file.h
@@ -24,7 +24,7 @@ namespace cc
 class file
 {
 public:
-  file(const quvi::media&, const quvi::url&, const int);
+  file(const quvi::media&);
 
   inline file(): _initial_length(0), _nothing_todo(false) { }
 
@@ -39,9 +39,9 @@ public:
       return *this;
     }
 public:
-  bool write(const quvi::url&, void*) const;
+  bool write(const quvi::media&, void*) const;
 public:
-  std::string to_s(const quvi::url&) const;
+  std::string to_s(const quvi::media&) const;
   inline const std::string& title() const { return _title; }
   inline const std::string& path() const  { return _path; }
   inline const std::string& name() const  { return _name; }
@@ -50,7 +50,7 @@ public:
 public:
   static double exists(const std::string&);
 private:
-  void _init(const quvi::media&, const quvi::url&, const int);
+  void _init(const quvi::media&);
 
   inline bool _should_continue() const { return _initial_length >0; }
 

src/cc/progressbar.cpp
@@ -76,9 +76,9 @@ static size_t get_term_width()
 
 namespace po = boost::program_options;
 
-progressbar::progressbar(const file& f, const quvi::url& u)
+progressbar::progressbar(const file& f, const quvi::media& m)
   : _update_interval(1),
-    _expected_bytes(u.content_length()),
+    _expected_bytes(m.content_length()),
     _initial_bytes(f.initial_length()),
     _time_started(0),
     _last_update(0),

src/cc/progressbar.h
@@ -30,7 +30,7 @@ class progressbar
 public:
   enum mode { normal = 0, dotline, simple };
 public:
-  progressbar(const file&, const quvi::url&);
+  progressbar(const file&, const quvi::media&);
   int update(double);
   void finish();
 private:

src/cc/util.h
@@ -18,12 +18,6 @@
 #ifndef cclive_util_h
 #define cclive_util_h
 
-namespace quvi
-{
-class media;
-class url;
-} // namespace quvi
-
 namespace cc
 {
 

commit b57e47beec029e3affb555745b2d3899036b5760

FIX: Regression in retrying introduced by 4b118378
The commit 4b118378 introduced a case of an "if-statement with an empty
body", causing the program to attempt retrying with both succeeded and
failed transfers (when --no-download was not used).  In doing so, the
program would also completely skip invoking the --exec command with the
succeeded transfers (when retried).

Affected versions:
  v0.7.12

Signed-off-by: Toni Gundogdu <legatvs@gmail.com>

src/cc/get.cpp
@@ -76,8 +76,8 @@ void get(quvi::media& media, void *curl)
 
       if (!no_download)
         {
-          if (!file.write(media, curl));
-          continue; // Retry.
+          if (!file.write(media, curl))
+            continue; // Retry.
 
           if (exec)
             cc::exec(file);